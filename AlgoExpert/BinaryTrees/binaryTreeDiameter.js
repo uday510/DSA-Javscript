a749095cca352dc3dcd86a40f4ec3835a5a111e1
Uday <budayteja009@gmail.com>
2022-04-17 12:46:29 +0530

    Added codes

--- AlgoExpert/BinaryTrees/binaryTreeDiameter.js
+++ AlgoExpert/BinaryTrees/binaryTreeDiameter.js
@@ -1,34 +1,35 @@
 class BinaryTree {
-  constructor(value) {
-    this.value = value;
-    this.left = null;
-    this.right = null;
-  }
+	constructor(value) {
+		this.value = value;
+		this.left = null;
+		this.right = null;
+	}
 }
 
+//! O(n) time | O(h) space
 function binaryTreeDiameter(tree) {
 	return getTreeInfo(tree).diameter;
 }
+
 function getTreeInfo(tree) {
-	
-	if(tree === null) {
-		return new TreeInfo(0, 0);
-	}
-	
+
+	if(!tree) return new TreeInfo(0, 0);
+
 	const leftTreeInfo = getTreeInfo(tree.left);
 	const rightTreeInfo = getTreeInfo(tree.right);
-	
-	const longestPathThroughRoot = leftTreeInfo.height + rightTreeInfo.height;
-	const maxDiamterSoFar = Math.max(leftTreeInfo.diameter, rightTreeInfo.diameter);
-	const currentDiameter = Math.max(longestPathThroughRoot, maxDiamterSoFar);
+
+	const longestPathThroughRoot = leftTreeInfo.height + rightSubTree.height;
+	const maxDiameterSoFar = Math.max(leftTreeInfo.diameter, rightTreeInfo.diameter);
+
+	const currentDiameter = Math.max(maxDiameterSoFar, longestPathThroughRoot);
 	const currentHeight = 1 + Math.max(leftTreeInfo.height, rightTreeInfo.height);
-	
-	return  new TreeInfo(currentDiameter, currentHeight);
+
+	return new TreeInfo(currentDiameter, currentHeight);
 }
-class TreeInfo
- {
-	 constructor(diameter, height) {
-		 this.diameter = diameter;
-		 this.height = height;
-	 }
- }
+
+class TreeInfo {
+	constructor(diameter, height) {
+		this.diameter = diameter;
+		this.height = height;
+	}
+}
