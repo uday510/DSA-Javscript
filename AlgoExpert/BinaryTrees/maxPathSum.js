ae67b50d708980640ed3b4598b6f47545a8ccc9d
Uday <budayteja009@gmail.com>
2022-04-19 17:04:00 +0530

    Added codes

--- AlgoExpert/BinaryTrees/maxPathSum.js
+++ AlgoExpert/BinaryTrees/maxPathSum.js
@@ -1,54 +1,21 @@
-//!12/02/2022
-class Node {
-    constructor(data) {
-        this.data = data;
-        this.left = null;
-        this.right = null;
-    }
+//! O(n) time | O(logn) space
 
+function maxPathSum(tree) {
+    const [_, maxSum] = findMaxSum(tree);
+    return maxSum;
 }
 
-class maxSum {
-    constructor(data) {
-        this.data = data;
-    }
+function findMaxSum(tree) {
+    if(!tree) return [-Infinity, -Infinity];
+
+    const [leftMaxSumAsBranch, leftMaxPathSum] = findMaxSum(tree.left);
+    const [rightMaxSumAsBranch, rightMaxPathSum] = findMaxSum(tree.right);
+    const maxChildSumAsBranch = Math.max(leftMaxSumAsBranch, rightMaxSumAsBranch);
+
+    const {value} = tree.value;
+    const maxSumAsBranch = Math.max(maxChildSumAsBranch + value, value);
+    const maxSumAsRootNode = Math.max(leftMaxSumAsBranch + value + rightMaxSumAsBranch, maxSumAsBranch);
+    const maxPathSum = Math.max(leftMaxPathSum, rightMaxPathSum, maxSumAsRootNode);
+
+    return [maxChildSumAsBranch, maxPathSum];
 }
-
-function maxPathSum(root) {
-    if(root === null) return 0;
-
-   
-    const leftSum = maxPathSum(root.left);
-    const rightSum = maxPathSum(root.right);
-
-
-    let sum = Math.max(leftSum, rightSum);
-    
-
-    
-
-    return sum;
-
-}
-
-const root = new Node(1);
-
-root.left = new Node(2);
-root.right = new Node(3);
-
-root.left.left = new Node(4);
-root.left.right = new Node(5);
-
-root.right.left = new Node(6);
-root.right.right = new Node(7);
-
-
-
-console.log(maxPathSum(root));
-
-
-
-
-
-
-
